72d9e0371b16adacd1098a0263d09bb0fb53f6c9
Nick Palenchar <nickpal@nickpalenchar.com>
2023-02-22 07:58:32 -0500

    404 and cleanup test components (#57)

--- scripts/components/src/Another.tsx
+++
@@ -1,8 +0,0 @@
-import type { Component } from "solid-js";
-
-const Another: Component = () => {
-  return <div>hello from solid AGAIN!</div>;
-};
-
-// @ts-expect-error
-window._componentAnother = Another;
--- scripts/components/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from "solid-js";
-
-const App: Component = () => {
-  console.log("hello????");
-  return <div>hello from solid!</div>;
-};
-
-window._componentApp = App;
-export default App;
